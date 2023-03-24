d0d49efbb3cf2dc011c5101f996cd4a985769a00
Milan <milangru@gmail.com>
2023-03-24 11:34:06 +0100

    Adding high score to file functionality'

--- scoreboard.py
+++ scoreboard.py
@@ -12,17 +12,25 @@ class Scoreboard(Turtle):
         self.goto(0, 260)
         self.hideturtle()
         self.update_scoreboard()
+        with open("high_scores.txt", mode="w") as file:
+            file.write("High scores:")
             
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)  
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            self.write_high_score()
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
+    def write_high_score(self):
+        with open("high_scores.txt", mode="a") as file:
+            file.write("\n")
+            file.write(str(self.highscore))
